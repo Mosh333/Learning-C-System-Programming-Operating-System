3e47e486d78d40ab445f157c9fdf91e659bcebdb
Moshiur <howladermoshiur@gmail.com>
2023-01-15 17:17:06 -0500

    Finished Q7, was annoying but finally worked

--- System Programming/2SH4_labs/lab2/q6_7/main.c
+++ System Programming/2SH4_labs/lab2/q6_7/main.c
@@ -16,6 +16,7 @@ int main(int argc, char *argv[]) {
     int length1 = countComponents(source1,size1);
     int val1[length1];
     int pos1[length1];
+    printf("val1 and pos1\n");
     efficient(source1,val1,pos1,size1);
     reconstruct(newSource1, length1, val1, pos1, size1);
     // for (size_t i = 0; i < size1; i++)
@@ -30,6 +31,7 @@ int main(int argc, char *argv[]) {
     int length2 = countComponents(source2,size2);
     int val2[length2];
     int pos2[length2];
+    printf("val2 and pos2\n");
     efficient(source2,val2,pos2,size2);
     reconstruct(newSource2, length2, val2, pos2, size2);
     // for (size_t i = 0; i < size2; i++)
@@ -44,6 +46,7 @@ int main(int argc, char *argv[]) {
     int length3 = countComponents(source3,size3);
     int val3[length3];
     int pos3[length3];
+    printf("val3 and pos3\n");
     efficient(source3,val3,pos3,size3);
     reconstruct(newSource3, length3, val3, pos3, size3);
     // for (size_t i = 0; i < size3; i++)
@@ -55,17 +58,22 @@ int main(int argc, char *argv[]) {
     //uniquePos=6
     int addEffVal4[6];
     int addEffPos4[6];
-    addEff(val1, val2, addEffVal4, pos1, pos2, addEffPos4, 3, 5);
+    // addEff(val1, val2, addEffVal4, pos1, pos2, addEffPos4, 3, 5);
+    addEff(val2, val1, addEffVal4, pos2, pos1, addEffPos4, 5, 3);
     printf("\n*********************\n");
-    // //pos1 = [2 4 7] + pos2 = [0 8]
-    // int addEffVal4[5+];
-    // int addEffPos4[];
-    // addEff(val1,val2);
-    // printf("\n*********************\n");
+    // //pos1 = [2 4 7] + pos3 = [0 8]
+    //uniquePos=5
+    int addEffVal5[5];
+    int addEffPos5[5];
+    addEff(val1,val3, addEffVal5, pos1, pos3, addEffPos5, 3, 2);
+    // addEff(val3, val1, addEffVal5, pos3, pos1, addEffPos5, 2, 3);
+    printf("\n*********************\n");
     // //pos1 = [1 4 7 9 10] + pos2 = [0 8]
-    // int addEffVal4[5+];
-    // int addEffPos4[];
-    // addEff(val1,val2);
+    int addEffVal6[7];
+    int addEffPos6[7];
+    // addEff(val2,val3, addEffVal6, pos2, pos3, addEffPos6, 5, 2);
+    addEff(val3,val2, addEffVal6, pos3, pos2, addEffPos6, 2, 5);
+    printf("\n*********************\n");
 
    return 0;
 }
--- System Programming/2SH4_labs/lab2/q6_7/sparseVector.c
+++ System Programming/2SH4_labs/lab2/q6_7/sparseVector.c
@@ -2,25 +2,28 @@
 #include <math.h>
 #include "sparseVector.h"
 
-//as per https://stackoverflow.com/questions/37538/how-do-i-determine-the-size-of-my-array-in-c
-//unless working with pointers or strings, just pass the length
-int countComponents(int source[], int length){
-    int counter=0;
+// as per https://stackoverflow.com/questions/37538/how-do-i-determine-the-size-of-my-array-in-c
+// unless working with pointers or strings, just pass the length
+int countComponents(int source[], int length)
+{
+    int counter = 0;
     /*  below does not work, see SO link above
     printf("size is: %d\n", sizeof(source)/sizeof(int));
     */
-    
+
     for (size_t i = 0; i < length; i++)
     {
-        if(source[i]!=0){
+        if (source[i] != 0)
+        {
             counter++;
         }
     }
-    
+
     return counter;
 }
 
-void efficient(const int source[], int val[], int pos[], int size){
+void efficient(const int source[], int val[], int pos[], int size)
+{
     printf("The original array: ");
     for (size_t i = 0; i < size; i++)
     {
@@ -28,12 +31,13 @@ void efficient(const int source[], int val[], int pos[], int size){
     }
     printf("\n");
 
-    int counter=0;
+    int counter = 0;
     for (size_t i = 0; i < size; i++)
     {
-        if(source[i]!=0){
-            val[counter]=source[i];
-            pos[counter]=i;
+        if (source[i] != 0)
+        {
+            val[counter] = source[i];
+            pos[counter] = i;
             printf("Val %d is at position %d\n", val[counter], pos[counter]);
             counter++;
         }
@@ -52,7 +56,8 @@ void efficient(const int source[], int val[], int pos[], int size){
     printf("\n");
 }
 
-void reconstruct(int source[], int m, const int val[], const int pos[], int n){
+void reconstruct(int source[], int m, const int val[], const int pos[], int n)
+{
     for (size_t i = 0; i < n; i++)
     {
         source[i] = 0;
@@ -68,90 +73,191 @@ void reconstruct(int source[], int m, const int val[], const int pos[], int n){
     {
         printf("%d ", source[i]);
     }
-    printf("\n");
 }
 
-void addEff(int val1[], int val2[], int val3[], int pos1[], int pos2[], int pos3[], int k1, int k2){
-    //uniquePos will be the size of 
+void addEff(int val1[], int val2[], int val3[], int pos1[], int pos2[], int pos3[], int k1, int k2)
+{
+    // uniquePos will be the size of
     int uniquePos = 0;
     int searchPos = 0;
-    int found = -1;
-    if(k1==0 || k2==0){
+    int pos1Pointer = 0;
+    int pos2Pointer = 0;
+    int pos1Exhausted = -1;
+    int pos2Exhausted = -1;
+    if (k1 == 0 || k2 == 0)
+    {
         printf("Both k1 and k2 must be at least 1!\n");
-    //the below branches finds the number of uniquePos
-    //find the number of uniquePos in this branch statement
-    //way I written below, you set uniquePos to max of (k1,k2) then iterate through the smaller pos[] list to determine the additional
-    //number of uniquePos there is in addition to the max(k1,k2)
-    //in retrospect, you can just set uniquePos to either or then search then increment and it would work
-    }else if(k1>k2){
-        int shorterArray = k2;
-        int longerArray = k1;
-        uniquePos = k1; //gurantee of at least k1 unique entries
-        //here from 0 to k2 entries can be unique in addition to k1
-        for (size_t i = 0; i < shorterArray; i++)
-        {
-            searchPos = pos2[i];
-            for (size_t j = 0; j < longerArray; j++)
-            {
-                if(pos1[j]==searchPos){
-                    found=1;
-                }
-            }
+        // the below branches finds the number of uniquePos
+        // find the number of uniquePos in this branch statement
+        // way I written below, you set uniquePos to max of (k1,k2) then iterate through the smaller pos[] list to determine the additional
+        // number of uniquePos there is in addition to the max(k1,k2)
+        // in retrospect, you can just set uniquePos to either or then search then increment and it would work
+    }
+    else
+    {
+        // uniquePos must be at most k1+k2 entries
+        // if there are duplicates between the two, the pair counts as one entry
+        uniquePos = k1 + k2;
+        // printf("uniquePos is: %d k1 %d k2 %d\n", uniquePos, k1, k2);
 
-            if(found==-1){
-                //unique to pos2
-                uniquePos++;
-            }else{
-                //reset flag
-                found=-1;
-            }
-        }
-        
-    }else if(k1==k2){
-        uniquePos = k1; //gurantee of at least k1 unique entries
-        //here from 0 to k2 entries can be unique in addition to k1
         for (size_t i = 0; i < k1; i++)
         {
             searchPos = pos1[i];
             for (size_t j = 0; j < k2; j++)
             {
-                if(pos2[j]==searchPos){
-                    found=1;
+                // printf("pos1 %d pos2 %d searchPos %d\n", pos1[i], pos2[j], searchPos);
+                if (pos2[j] == searchPos)
+                {
+                    // duplicate
+                    uniquePos--;
                 }
-            }
-
-            if(found==-1){
-                //unique to pos2
-                uniquePos++;
-            }else{
-                //reset flag
-                found=-1;
             }
         }
-    }else if(k2>k1){
-        int shorterArray = k1;
-        int longerArray = k2;
-        uniquePos = k2; //gurantee of at least k2 unique entries
-        //here from 0 to k1 entries can be unique in addition to k2
-        for (size_t i = 0; i < shorterArray; i++)
+
+        // both pos arrays are gurantteed to have at least one entry
+
+        // printf("Val3 array is: \n");
+
+        for (size_t i = 0; i < uniquePos; i++)
         {
-            searchPos = pos1[i];
-            for (size_t j = 0; j < longerArray; j++)
+            // printf("pos1[x]: %d pos2[y]: %d pos1Pointer %d  pos2Pointer %d val1[x] %d val2[y] %d k1 %d k2 %d ", pos1[pos1Pointer], pos2[pos2Pointer], pos1Pointer, pos2Pointer, val1[pos1Pointer], val2[pos2Pointer], k1, k2);
+
+            if (pos1Exhausted == 1)
             {
-                if(pos2[j]==searchPos){
-                    found=1;
+                // previous iteration, we have reached the max index for pos1
+                // compare logic is modified here
+                // printf("In pos1Exhausted\n");
+                if (pos1[pos1Pointer] < pos2[pos2Pointer])
+                {
+                    // confirm that pos1's last entry indeed is less thatn pos2's current entry,
+                    // if so, safe to continue appending the rest of pos2 into pos3
+                    val3[i] = val2[pos2Pointer];
+                    pos3[i] = pos2[pos2Pointer];
+                    pos2Pointer++;
                 }
             }
-
-            if(found==-1){
-                //unique to pos2
-                uniquePos++;
-            }else{
-                //reset flag
-                found=-1;
+            else if (pos2Exhausted == 1)
+            {
+                // previous iteration, we have reached the max index for pos2
+                // compare logic is modified here
+                // printf("In pos2Exhausted\n");
+                if (pos2[pos2Pointer] < pos1[pos1Pointer])
+                {
+                    // confirm that pos1's last entry indeed is less thatn pos2's current entry,
+                    // if so, safe to continue appending the rest of pos2 into pos3
+                    val3[i] = val1[pos1Pointer];
+                    pos3[i] = pos1[pos1Pointer];
+                    pos1Pointer++;
+                }
+                // below is for posExhausted<1
             }
-        }        
+            else if (pos1[pos1Pointer] == pos2[pos2Pointer])
+            {
+                // equal positions, superimpose or add them up
+                val3[i] = val1[pos1Pointer] + val2[pos2Pointer];
+                pos3[i] = pos1[pos1Pointer];
+                // printf("val3 became: %d ", val3[i]);
+                if (pos1Pointer + 1 < k1)
+                {
+                    // ensure to not be out of bound
+                    pos1Pointer++;
+                }
+                if (pos2Pointer + 1 < k2)
+                {
+                    // ensure to not be out of bound
+                    pos2Pointer++;
+                }
+                if (pos1Pointer == k1 - 1)
+                {
+                    // relying on incrementing twice to get a twice in a row event
+                    //-1 -> 0 -> 1
+                    pos1Exhausted++;
+                }
+                if (pos2Pointer == k2 - 1)
+                {
+                    // relying on incrementing twice to get a twice in a row event
+                    //-1 -> 0 -> 1
+                    pos2Exhausted++;
+                }
+
+                // printf(" branch a\n");
+            }
+            else if (pos1[pos1Pointer] < pos2[pos2Pointer])
+            {
+                val3[i] = val1[pos1Pointer];
+                pos3[i] = pos1[pos1Pointer];
+                // printf("val3 became: %d ", val3[i]);
+                // printf("    val3 became: %d because pos1[pos1Pointer] %d pos2[pos2Pointer] %d", val3[i], pos1[pos1Pointer], pos2[pos2Pointer]);
+                if (pos1Pointer + 1 < k1)
+                {
+                    // ensure to not be out of bound
+                    pos1Pointer++;
+                }
+                if (pos1Pointer == k1 - 1)
+                {
+                    // relying on incrementing twice to get a twice in a row event
+                    //-1 -> 0 -> 1
+                    pos1Exhausted++;
+                }
+                // printf(" branch b\n");
+            }
+            else if (pos2[pos2Pointer] < pos1[pos1Pointer])
+            {
+                val3[i] = val2[pos2Pointer];
+                pos3[i] = pos2[pos2Pointer];
+                // printf("val3 became: %d ", val3[i]);
+                if (pos2Pointer + 1 < k2)
+                {
+                    // ensure to not be out of bound
+                    pos2Pointer++;
+                }
+                if (pos2Pointer == k2 - 1)
+                {
+                    // relying on incrementing twice to get a twice in a row event
+                    //-1 -> 0 -> 1
+                    pos2Exhausted++;
+                }
+                // printf(" branch c\n");
+            }
+        }
+
+        // printf("\n");
+        printf("val1 array: ");
+        for (size_t i = 0; i < k1; i++)
+        {
+            printf("%d ", val1[i]);
+        }
+        printf("\n");
+        printf("val2 array: ");
+        for (size_t i = 0; i < k2; i++)
+        {
+            printf("%d ", val2[i]);
+        }
+        printf("\n");
+        printf("pos1 array: ");
+        for (size_t i = 0; i < k1; i++)
+        {
+            printf("%d ", pos1[i]);
+        }
+        printf("\n");
+        printf("pos2 array: ");
+        for (size_t i = 0; i < k2; i++)
+        {
+            printf("%d ", pos2[i]);
+        }
+        printf("\n");
+        printf("val3: ");
+        for (size_t i = 0; i < uniquePos; i++)
+        {
+            printf("%d ", val3[i]);
+        }
+        printf("\n");
+        printf("pos3: ");
+        for (size_t i = 0; i < uniquePos; i++)
+        {
+            printf("%d ", pos3[i]);
+        }
     }
-    
-    printf("Unique Pos Entries: %d\n", uniquePos);
+
+    printf("\nUnique Pos Entries: %d", uniquePos);
 }
