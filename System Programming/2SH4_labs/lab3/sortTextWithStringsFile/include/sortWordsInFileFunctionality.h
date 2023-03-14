ab5ddee86697e5c93a9face9cf47d40163157072
Moshiur <howladermoshiur@gmail.com>
2023-03-14 01:54:05 -0400

    Cleaned Up lab 3
    Better readability

--- System Programming/2SH4_labs/lab3/sortTextWithStringsFile/include/sortWordsInFileFunctionality.h
+++ System Programming/2SH4_labs/lab3/sortTextWithStringsFile/include/sortWordsInFileFunctionality.h
@@ -11,25 +11,8 @@ void sort_words(char **wordList, int size);
 //use bubble sort
 void sort2_words(char **wordList, int size);
 
-//
+//compare the lexicographical order of the words
 void compareLexicographicalOrder(char **wordList, int index1, int index2);
 
-// int find(int idNo, student **list, int size);
-
-// void input_grades( char *filename, student **list, int size );
-
-// void compute_final_course_grades(student **list, int size);
-
-// void output_final_course_grades(char *filename, student **list, int size);
-
-// void print_list( student **list, int size);
-
-// void withdraw(int idNo, student **list, int *sizePtr);
-
-// void destroy_list(student **list, int *sizePtr);
-
-// void practiceSyntax();
-
-// void illustratePointerMechanics();
 
 #endif
